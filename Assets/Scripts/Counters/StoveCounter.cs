4951f3543d6098e04407a87482f4ff11d912004c
eleanoracd <reinsword.work@outlook.com>
2024-09-01 17:10:10 +0700

    Polishing

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -218,4 +218,9 @@ public class StoveCounter : BaseCounter, IHasProgress
         }
         return null;
     }
+
+    public bool IsFried()
+    {
+        return state == State.Fried;
+    }
 }
--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -21,7 +21,7 @@ public class KitchenGameManager : MonoBehaviour
     private State state;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 10f;
+    private float gamePlayingTimerMax = 100f;
     private bool isGamePaused = false;
 
     private void Awake()
